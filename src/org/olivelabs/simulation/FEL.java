bb16f8299d41cd3653c7e5830b5a310c9cc66096
Arshad Ansari <arshadansari27@gmail.com>
2012-03-21 00:34:50 +0100

    Removing redundant stuff!!

--- src/org/olivelabs/simulation/FEL.java
+++
@@ -1,7 +0,0 @@
-package org.olivelabs.simulation;
-
-import java.util.PriorityQueue;
-
-public class FEL<Event> extends PriorityQueue<Event> {
-
-}
--- src/org/olivelabs/simulation/SimulationClock.java
+++
@@ -1,5 +0,0 @@
-package org.olivelabs.simulation;
-
-public class SimulationClock {
-	static long CurrentTime = 0;
-}
